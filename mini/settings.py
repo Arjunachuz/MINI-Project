fb7fbc79ecf33069e08ae3a29755c9f6571f9cd5
unknown <you@example.com>
2022-10-29 11:53:41 +0530

    updated settings

--- mini/settings.py
+++ mini/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3r=mht@xdtso)b0f9&msy@!rj*h!)b72c+_*kn$1&)!#pvbww1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [' ec2-52-68-145-111.ap-northeast-1.compute.amazonaws.com']
 
 
 # Application definition
